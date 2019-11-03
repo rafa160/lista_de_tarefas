1c1911c1fb6406af872381feefa3a4969ddf3d25
rafaelpaz <rafaelgoes@infarma.com>
2019-11-02 21:05:08 -0300

    Adiciona opcao de deletar e snack para desfazer

--- lib/main.dart
+++ lib/main.dart
@@ -4,36 +4,35 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main (){
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   final _controlador = TextEditingController();
 
   List _listaDeTarefas = [];
-
+  Map<String, dynamic> _ultimoDeletado;
+  int _ultimoDeletadoPosicao;
 
   @override
   void initState() {
     super.initState();
-    _readData().then((data){
+    _readData().then((data) {
       setState(() {
         _listaDeTarefas = json.decode(data);
       });
-
     });
   }
 
-  void _addTarefa(){
+  void _addTarefa() {
     setState(() {
       Map<String, dynamic> novaTarefa = Map();
       novaTarefa["title"] = _controlador.text;
@@ -42,7 +41,6 @@ class _HomeState extends State<Home> {
       _listaDeTarefas.add(novaTarefa);
       _saveData();
     });
-
   }
 
   @override
@@ -53,14 +51,14 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.brown,
         centerTitle: true,
       ),
-      body:  Column(
+      body: Column(
         children: <Widget>[
           Container(
             padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child:TextFormField(
+                  child: TextFormField(
                     controller: _controlador,
                     decoration: InputDecoration(
                       labelText: "Nova Tarefa",
@@ -70,8 +68,7 @@ class _HomeState extends State<Home> {
                 ),
               ],
             ),
-
-      ),
+          ),
           SizedBox(
             height: 10,
           ),
@@ -79,12 +76,12 @@ class _HomeState extends State<Home> {
             children: <Widget>[
               Container(
                 margin: EdgeInsets.only(left: 120),
-                padding:  EdgeInsets.fromLTRB(10, 2, 10, 2),
+                padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
                 child: RaisedButton(
                   color: Colors.brown,
                   child: Text("Adicionar"),
                   textColor: Colors.white,
-                  onPressed: (){
+                  onPressed: () {
                     _addTarefa();
                   },
                 ),
@@ -92,34 +89,72 @@ class _HomeState extends State<Home> {
             ],
           ),
           Expanded(
-            child: _listaDeTarefasNovas(),
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 20),
+                itemCount: _listaDeTarefas.length,
+                itemBuilder: buildItem),
           ),
         ],
       ),
     );
   }
 
-  Widget _listaDeTarefasNovas(){
-    return ListView.builder(
-        padding: EdgeInsets.only(top: 20),
-        itemCount: _listaDeTarefas.length,
-        itemBuilder:(context, index){
-          return CheckboxListTile(
-            activeColor: Colors.green,
-           title: Text(_listaDeTarefas[index]["title"]),
-            value: _listaDeTarefas[index]["Ok"],
-            secondary: CircleAvatar(
-              backgroundColor: Colors.brown,
-              foregroundColor: Colors.white,
-              child:Icon(_listaDeTarefas[index]["Ok"] ? Icons.check : Icons.error) ,),
-            onChanged: (c){
-             setState(() {
-               _listaDeTarefas[index]["Ok"] = c;
-               _saveData();
-             });
-            },
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          )),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        activeColor: Colors.green,
+        title: Text(_listaDeTarefas[index]["title"]),
+        value: _listaDeTarefas[index]["Ok"],
+        secondary: CircleAvatar(
+          backgroundColor: Colors.brown,
+          foregroundColor: Colors.white,
+          child: Icon(_listaDeTarefas[index]["Ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _listaDeTarefas[index]["Ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction){
+        setState(() {
+          _ultimoDeletado = Map.from(_listaDeTarefas[index]);
+          _ultimoDeletadoPosicao = index;
+          _listaDeTarefas.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa ${_ultimoDeletado["title"]} removida!"),
+            action: SnackBarAction(label: "Desfazer?",
+                onPressed: (){
+                   setState(() {
+                     _listaDeTarefas.insert(_ultimoDeletadoPosicao, _ultimoDeletado);
+                     _saveData();
+                   });
+                }),
+            duration: Duration(seconds: 2),
           );
-    });
+          Scaffold.of(context).showSnackBar(snack);
+        });
+
+      },
+    );
+  }
+
+  Widget _deleteItem(context, index) {
+    return null;
   }
 
   Future<File> _getFile() async {
@@ -134,12 +169,11 @@ class _HomeState extends State<Home> {
   }
 
   Future<String> _readData() async {
-    try{
+    try {
       final file = await _getFile();
       return file.readAsString();
-    } catch (e){
+    } catch (e) {
       return null;
     }
   }
-
 }
